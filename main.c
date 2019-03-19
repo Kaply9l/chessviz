0f118ebd88f55d35fc48f8bb1e46e52f95b8ada8
Kaply9l <kislov.igor@list.ru>
2019-03-19 13:14:32 +0700

    Edit .clang-format

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 
 void showdesk(char desk[8][8])
 {
-    int i,j;
+    int i, j;
     printf("\n");
     for (i = 7; i >= 0; i--) {
         printf("%d|", i + 1);
+
         for (j = 0; j < 8; j++) {
             printf(" %c", desk[i][j]);
         }
-    printf("\n");
+        printf("\n");
     }
     printf("   _______________\n");
     printf("   A B C D E F G H\n");
@@ -17,15 +18,14 @@ void showdesk(char desk[8][8])
 
 int main()
 {
-	char desk[8][8] = {
- 		"rhbqkbhr",
- 		"pppppppp",
- 		"........",
- 		"........",
- 		"........",
- 		"........",
- 		"PPPPPPPP",
- 		"RHBQKBHR" };
+    char desk[8][8] = {"rhbqkbhr",
+                       "pppppppp",
+                       "........",
+                       "........",
+                       "........",
+                       "........",
+                       "PPPPPPPP",
+                       "RHBQKBHR"};
     showdesk(desk);
     return 0;
 };
